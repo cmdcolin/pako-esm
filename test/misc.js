f54dcfd5895db241df1d3c48dff0b95235f75974
Colin <colin.diesh@gmail.com>
2025-11-09 08:48:51 -0500

    Misc

--- test/misc.js
+++ test/misc.js
@@ -1,31 +1,21 @@
-'use strict';
+'use strict'
 
-
-import { assert, describe, it } from 'vitest';
-import fs from 'fs';
-import path from 'path';
-import * as pako from '../lib/main';
-import { cmpBuf as cmp } from './helpers';
+import { assert, describe, it } from 'vitest'
+import fs from 'fs'
+import path from 'path'
+import * as pako from '../lib/main'
+import { cmpBuf as cmp } from './helpers'
 
 describe('ArrayBuffer', function () {
-
-  var file   = path.join(__dirname, 'fixtures/samples/lorem_utf_100k.txt');
-  var sample = new Uint8Array(fs.readFileSync(file));
-  var deflated = pako.deflate(sample);
+  var file = path.join(__dirname, 'fixtures/samples/lorem_utf_100k.txt')
+  var sample = new Uint8Array(fs.readFileSync(file))
+  var deflated = pako.deflate(sample)
 
   it('Deflate ArrayBuffer', function () {
-    assert.ok(cmp(deflated, pako.deflate(sample.buffer)));
-  });
+    assert.ok(cmp(deflated, pako.deflate(sample.buffer)))
+  })
 
   it('Inflate ArrayBuffer', function () {
-    assert.ok(cmp(sample, pako.inflate(deflated.buffer)));
-  });
-
-  it('Simplified minified version test', function () {
-    // At some point minifier started to corrupt str2buf function
-    // https://github.com/nodeca/pako/issues/161#issuecomment-468420555
-    var minified = require('../dist/pako.min.js');
-
-    assert.ok(cmp(minified.deflate('→'), pako.deflate('→')));
-  });
-});
+    assert.ok(cmp(sample, pako.inflate(deflated.buffer)))
+  })
+})
